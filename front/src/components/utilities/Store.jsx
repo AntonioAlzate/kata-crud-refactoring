45a7343bf40e93946a12432231d7a599726ea96e
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-27 23:11:09 -0500

    Se pasa lo que se tenia en el App.js a archivos separados para tener más orden, conserva el mismo funcionamiento.

---
+++ front/src/components/utilities/Store.jsx
@@ -0,0 +1,22 @@
+
+import React, {createContext, useReducer} from 'react';
+import Reducer from './Reducer';
+
+const initialState = {
+    todo: { list: [], item: {} },
+    group: { listGroup: [], itemGroup: {}}
+  };
+
+const Store = createContext(initialState);
+
+export const StoreProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  return <Store.Provider value={{ state, dispatch }}>
+      {children}
+  </Store.Provider>
+  
+  }
+
+  export default Store;
